3a282324a3fd28d8c9d5cb9188668b64dae9518e
tini2n <tini2n@gmail.com>
2022-03-12 14:02:05 +0200

    Logo component props model

--- src/ui/molecules/Logo/index.tsx
+++ src/ui/molecules/Logo/index.tsx
@@ -1,8 +1,9 @@
 import { S } from './index.styled';
+import { Props } from './index.model';
 
 import LogoIcon from 'lib/icons/decard_logo_wordmark.svg';
 
-const Logo = ({ ...rest }) => {
+const Logo: React.FunctionComponent<Props> = ({ ...rest }) => {
 	return (
 		<S.Logo {...rest} href='/'>
 			<LogoIcon />
